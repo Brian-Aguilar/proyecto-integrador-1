c85319660cf174e953ca9e2f7ff5c8bffdb57499
Brian Junior Aguilar Davila <aguilar.16999@gmail.com>
2024-10-27 01:31:13 -0500

    Se agrega el servicio de Local

---
+++ src/main/java/com/example/integradorsi/services/LocalService.java
@@ -0,0 +1,33 @@
+package com.example.integradorsi.services;
+
+import com.example.integradorsi.DAO.DAOLocal;
+import com.example.integradorsi.models.Llocal;
+import java.util.List;
+
+public class LocalService {
+    
+    private DAOLocal localDAO;
+    
+    public LocalService(DAOLocal localDAO) {
+        this.localDAO = localDAO;
+    }
+    
+    public Llocal obtenerLocal(int id) {
+        return localDAO.getById(id);
+    }
+    public List<Llocal> obtenerLocales() {
+        return localDAO.getAll();
+    }
+    public void agregarLocal(Llocal local) {
+        localDAO.add(local);
+    }
+    public void actualizarLocal(Llocal local) {
+        localDAO.update(local);
+    }
+    public Llocal eliminarLocal(int id) {
+        return localDAO.delete(id);
+    }
+    public int cantidadDeLocales() {
+        return localDAO.size();
+    }
+}
